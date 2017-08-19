e0fbe8315303e79042ebc3298847e39d1e1ace28
Malik <mip12793@gmail.com>
2017-08-19 15:54:35 -0400

    half of edit page completed, also added a logo

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,36 @@
 class EventsController < ApplicationController
 	def index 
-		@events= Event.all
+		@events = Event.all
+		#should I store my venues in the index to show its values?
+		@venues = Venue.where(params[:id])
 	end
+
+	def edit 
+		@events = Event.find(params[:id])
+		puts @events.inspect
+	end
+
+	def update 
+		@events = Event.find(params[:id])
+		@events.update_attributes(name: params[:event][:name], date: params[:event][:date], alcohol_served: params[:event][:alcohol_served], venue_id: params[:event][:venue_id], band_id: params[:event][:band_id])
+		redirect_to action: 'index'
+	end
+
+	def destroy
+		@events=Event.find(params[:id])
+		@events.destroy
+		redirect_to action: 'index'
+	end
+
+	def new 
+		@Events = Event.new
+	end
+
+	def create
+		@Events = Event.create(name: params[:event][:name], date: params[:event][:date], alcohol_served: params[:event][:alcohol_served], venue_id: params[:event][:venue_id], band_id: params[:event][:band_id])
+		@Events.save
+		redirect_to action: 'index'
+	end
+
 	
 end
