eea29b523d031143046673c34bdad8455292ecc0
Malik <mip12793@gmail.com>
2017-08-19 16:39:04 -0400

    added structure for bands branch

--- app/controllers/bands_controller.rb
+++ app/controllers/bands_controller.rb
@@ -1,2 +1,37 @@
 class BandsController < ApplicationController
+	def index 
+		@bands = Band.all
+	end
+
+	def edit
+		@bands = Band.find(params[:id])
+		puts @bands.inspect
+	end
+
+	def update 
+		@bands = Band.find(params[:id])
+		@bands.update_attributes(name: params[:bands][:name], genre: params[:band][:genre], image: params[:image][:image], explicit_lyrics: params[:bands][:explicit_lyrics],)
+	end
+
+	def show
+		@bands = Band.find(params[:id])
+	end
+
+	def destroy
+		@bands = Band.find(params[:id])
+		@bands.destroy
+		redirect_to action: 'index'
+	end
+
+	def new
+		@bands = Band.new
+	end
+
+	def create
+		@bands = Band.create(name: params[:bands][:name], genre: params[:band][:genre], image: params[:image][:image], explicit_lyrics: params[:bands][:explicit_lyrics],)
+		@bands.save
+		redirect_to action: 'index'
+	end
+
+
 end
